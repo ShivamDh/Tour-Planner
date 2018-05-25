737af2d46780f2a9ae9e014e86936b81b33bb227
Shivam Dharme <shdh8888@yahoo.ca>
2018-05-24 21:55:39 -0400

    Fetch results from Google Distance Matrix API

--- MapsDistance.js
+++ MapsDistance.js
@@ -5,11 +5,58 @@ import GOOGLE_CREDENTIALS from './GoogleCredentials';
 var API_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?';
 
 var MapsDistance = function() {
-	this.get = function(args, callback) {
+	this.get = (args, callback) => {
 		var options = formatOptions(args);
 
-		
+		fetchData(options, (err, data) => {
+			if (err) {
+				return callback(err);
+			}
+
+			formatResults(data, options, (err, results) => {
+				if (err) return callback(err);
+				return callback(null, results);
+			});
+
+		});
 	}
+};
+
+var formatOptions = (args) => {
+	var options = {
+		origins: args.origins.join('|'),
+		destinations: args.destinations.join('|'),
+		mode: 'driving',
+		units: 'metric',
+		language: 'en',
+		key: GOOGLE_CREDENTIALS.key,
+		batchMode: true
+	};
+
+	if (!options.origins || !options.destinations) {
+		throw new Error('Argument Error: Locations are invalid');
+	}
+
+	return options;
+};
+
+var fetchData = (options, callback) => {
+  	fetch(API_URL + qs.stringify(options))
+		.then((response) => {
+			if(response.status != 200) {
+				let error = new Error(response.statusText);
+				error.response = response;
+				throw error;
+			}
+			return response;
+		})
+		.then((response) => response.json())
+		.then((response) => {
+			callback(null, response);
+		})
+		.catch ((error) => {
+			requestHasError(error, callback);
+		});
 };
 
 module.exports = new MapsDistance();
